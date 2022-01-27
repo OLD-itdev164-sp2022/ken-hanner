5d14d2c0b38b304ca1b0f586c99a7fffb9ffc7c4
Ken Hanner <ken.hanner@gmail.com>
2022-01-26 22:09:48 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Ken Hanner`,
+    contact: {
+      name: `Ken Hanner`,
+      company: `Hanner Time`,
+      address: `123 Main Street`,
+    },
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
