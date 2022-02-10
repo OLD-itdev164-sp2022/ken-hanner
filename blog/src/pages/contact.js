0a2fc3e43fd392bbbeaa5b28d6dfadf9e910615c
Ken Hanner <ken.hanner@gmail.com>
2022-02-10 17:43:29 -0600

    update Gatsby

--- blog/src/pages/contact.js
+++ blog/src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { Link, graphql }  from "gatsby"
+import { Link } from "gatsby"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import Img from "gatsby-image"
