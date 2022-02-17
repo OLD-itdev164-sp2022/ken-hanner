268bd4ab9446044fa900f6796cc1570dfadafa0a
Ken Hanner <ken.hanner@gmail.com>
2022-02-16 21:45:47 -0600

    midway A3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,7 @@
-import React from "react"
-import { Link, graphql }  from "gatsby"
+
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -12,6 +14,15 @@ const IndexPage = ({ data }) => (
       data.allContentfulBlogPost.edges.map(edge => (
         <li>
           <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage
+              image={edge.node.heroImage.gatsbyImageData}
+              alt="test"
+            />
+          </div>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
         </li>
       ))
     }
@@ -29,6 +40,18 @@ export const query = graphql`
           id
           title
           slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
       }
     }
